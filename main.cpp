e88d5ab936d40a155266819614e7b0ed143f905d
Quibilia <SirLevalioDaBurnshire@gmail.com>
2019-12-01 14:21:53 -0600

    Revision
    After coming back to this project after a very long time out of curiosity, I found many things that could've been optimized or rewritten to suit the purpose better. For one major thing, the program is now much less susceptible to overflow.

--- main.cpp
+++ main.cpp
@@ -1,146 +1,20 @@
-#include "main.h"
-
+#include <cstdlib>
+#include <iostream>
+#include <math.h>
 using namespace std;
 
-const double G = 0.00000000006674;
-const long long C_SQ = 89875517873681764;
+const double G = 0.0000000000667408;
+const long long C_SQ = 89875517873681764LL;
 
-string IToA(int n)
+struct BODY
 {
-	string cOut = "";
-	int begin = n;
-	int DIV;
-	int MOD;
-
-	if (n < 0)
-	{
-		begin = -n;
-	}
-
-	DIV = begin;
-
-	do
-	{
-		MOD = DIV % 10;
-		DIV /= 10;
-
-		cOut.insert(cOut.begin(), (char)(MOD + 48));
-	} while (DIV > 0);
-
-	if (n < 0)
-	{
-		cOut.insert(cOut.begin(), '-');
-	}
-
-	return cOut;
-}
-
-string DToA(double n)
-{
-	string cOut = "";
-	double begin = n;
-	int WHOLE;
-	double DECIMAL;
-	int DIV;
-	double MUL;
-	int MOD;
-
-	if (n < 0.0)
-	{
-		begin = -n;
-	}
-
-	WHOLE = (int)begin;
-	DECIMAL = begin - (double)WHOLE;
-
-	DIV = WHOLE;
-
-	do
-	{
-		MOD = DIV % 10;
-		DIV /= 10;
-
-		cOut.insert(cOut.begin(), (char)(MOD + 48));
-	} while (DIV > 0);
-
-	cOut.insert(cOut.end(), '.');
-
-	MUL = DECIMAL;
-
-	do
-	{
-		MUL *= 10.0;
-		DIV = (int)MUL;
-		MUL -= DIV;
-
-		cOut.insert(cOut.end(), (char)(DIV + 48));
-	} while (MUL > 0.0);
-
-	if (n < 0)
-	{
-		cOut.insert(cOut.begin(), '-');
-	}
-
-	return cOut;
-}
-
-int Pow(int x, int y)
-{
-	bool DIV = false;
-	int iOut = 1;
-	int n = y;
-
-	if (y < 0)
-	{
-		n = -y;
-		DIV = true;
-	}
-
-	for (int i = 0; i < n; i++)
-	{
-		if (DIV)
-		{
-			iOut /= x;
-		}
-		else
-		{
-			iOut *= x;
-		}
-	}
-
-	return iOut;
-}
-
-int AToI(string cS)
-{
-	int iOut = 0;
-	int msgLen = cS.length();
-
-	for (int i = 0; i < msgLen; i++)
-	{
-		iOut += (int)(cS[i] - 48) * Pow(10, (msgLen - (i + 1)));
-	}
-
-	return iOut;
-}
-
-double AToD(string cS)
-{
-	double dOut = 0.0;
-	int msgLen = cS.length();
-	int currentPow = msgLen;
-
-	for (int i = 0; i < msgLen; i++)
-	{
-		if (cS[i] != '.')
-		{
-			currentPow--;
-			dOut += (double)(int)(cS[i] - 48) * (double)(Pow(10, currentPow));
-		}
-	}
-
-	return dOut;
-}
+	long double MASS_BASE;
+	long double MASS_EXPONENT;
+	long double DISTANCE_FROM_CENTER;
+	long double SPEED;
+	long double U_BASE;
+	long double U_EXPONENT;
+};
 
 int main()
 {
@@ -148,64 +22,53 @@ int main()
 	int numBod = 0;
 	int locBod = 0;
 	int conBod = 0;
-	double orbSp = 0;
-	double conSp = 0;
-	int locDis = 0;
-	int conDis = 0;
-	long double Dg;
-	long double Dv;
-	long double Dr;
-	long double D;
-	long double DCon;
+	long double orbSp = 0;
+	long double conSp = 0;
+	long double locDis = 0;
+	long double conDis = 0;
+	long double Dg = 0.0;
+	long double Dv = 0.0;
+	long double Dr = 0.0;
+	long double D = 0.0;
+	long double DCon = 0.0;
 	BODY* bodies;
 
 	cout << "NUMBER OF BODIES IN SYSTEM > ";
-	getline(cin, sIn);
-	numBod = AToI(sIn);
+	cin >> numBod;
 	bodies = new BODY[numBod];
 
 	for (int i = 0; i < numBod; i++)
 	{
-		cout << "\nE-NOTATION BASE OF BODY " << IToA(i) << " MASS > ";
-		getline(cin, sIn);
-		bodies[i].MASS_BASE = AToD(sIn);
+		cout << "\nE-NOTATION BASE OF BODY " << i << " MASS > ";
+		cin >> bodies[i].MASS_BASE;
 
-		cout << "E-NOTATION EXPONENT OF BODY " << IToA(i) << " MASS > ";
-		getline(cin, sIn);
-		bodies[i].MASS_EXPONENT = AToI(sIn);
+		cout << "E-NOTATION EXPONENT OF BODY " << i << " MASS > ";
+		cin >> bodies[i].MASS_EXPONENT;
 
-		cout << "DISTANCE OF BODY " << IToA(i) << " FROM CENTER OF SYSTEM > ";
-		getline(cin, sIn);
-		bodies[i].DISTANCE_FROM_CENTER = AToI(sIn);
+		cout << "DISTANCE OF BODY " << i << " FROM CENTER OF SYSTEM > ";
+		cin >> bodies[i].DISTANCE_FROM_CENTER;
 
-		cout << "ORBITAL SPEED OF BODY " << IToA(i) << " > ";
-		getline(cin, sIn);
-		bodies[i].SPEED = AToI(sIn);
+		cout << "ORBITAL SPEED OF BODY " << i << " > ";
+		cin >> bodies[i].SPEED;
 	}
 
 	cout << "\nON WHICH BODY IS THE LOCAL OBSERVER? > ";
-	getline(cin, sIn);
-	locBod = AToI(sIn);
+	cin >> locBod;
 
 	cout << "ON WHICH BODY IS THE CONTROL OBSERVER? (-1 FOR OUTSIDE SYSTEM) > ";
-	getline(cin, sIn);
-	conBod = AToI(sIn);
+	cin >> conBod;
 
 	cout << "\nDISTANCE OF LOCAL OBSERVER FROM CENTER OF LOCAL BODY > ";
-	getline(cin, sIn);
-	locDis = AToI(sIn);
+	cin >> locDis;
 
 	cout << "DISTANCE OF CONTROL OBSERVER FROM CENTER OF CONTROL BODY (-1 FOR OUTSIDE SYSTEM) > ";
-	getline(cin, sIn);
-	conDis = AToI(sIn);
+	cin >> conDis;
 
 	cout << "\nSPEED OF LOCAL OBSERVER > ";
-	getline(cin, sIn);
-	orbSp = AToD(sIn);
+	cin >> orbSp;
 
 	cout << "SPEED OF CONTROL OBSERVER > ";
-	getline(cin, sIn);
-	conSp = AToD(sIn);
+	cin >> conSp;
 
 	for (int i = 0; i < numBod; i++)
 	{
@@ -259,42 +122,44 @@ int main()
 	long double LOC_U;
 	long double CON_U;
 
-	LOC_U = (long double)bodies[locBod].U_BASE;
-	LOC_U *= (long double)powl(10, bodies[locBod].U_EXPONENT);
-
-	CON_U = (long double)bodies[conBod].U_BASE;
-	CON_U *= (long double)powl(10, bodies[conBod].U_EXPONENT);
+	LOC_U = bodies[locBod].U_BASE;
+	CON_U = bodies[conBod].U_BASE;
 
 	UDIV = LOC_U;
 	UDIV *= 2.0;
+	if (bodies[locBod].U_EXPONENT > 12) { UDIV *= powl(10, 12L); }
+	else { UDIV *= powl(10, bodies[locBod].U_EXPONENT); }
 	UDIV /= C_SQ;
+	if (bodies[locBod].U_EXPONENT > 12) { UDIV *= powl(10, bodies[locBod].U_EXPONENT - 12L); }
 
-	VDIV = (long double)powl(bodies[locBod].SPEED, 2);
+	VDIV = powl(bodies[locBod].SPEED, 2);
 	VDIV /= C_SQ;
 
-	RDIV = (long double)powl(orbSp, 2);
+	RDIV = powl(orbSp, 2);
 	RDIV /= C_SQ;
 	RDIV *= 1.0 / (1.0 - UDIV);
 
 	Dg = 1.0 / sqrt(1.0 - UDIV);
 	Dv = 1.0 / sqrt(1.0 - VDIV);
 	Dr = 1.0 / sqrt(1.0 - RDIV);
-
-	cout << "\nTIME DILATION DUE TO GRAVITY AT LOCATION OF LOCAL OBSERVER : " << DToA(Dg) << endl;
-	cout << "TIME DILATION DUE TO MOTION OF LOCAL BODY : " << DToA(Dv) << endl;
-	cout << "TIME DILATION DUE TO MOTION OF LOCAL OBSERVER : " << DToA(Dr) << endl;
-
 	D = 1.0 / sqrt(fabs(1.0 - UDIV - VDIV - RDIV));
 
-	cout << "\nTOTAL TIME DILATION OF LOCAL OBSERVER : " << DToA(D) << endl;
+	printf_s("\nTIME DILATION DUE TO GRAVITY AT LOCATION OF LOCAL OBSERVER : %.16Lf\n", Dg);
+	printf_s("TIME DILATION DUE TO MOTION OF LOCAL BODY : %.16Lf\n", Dv);
+	printf_s("TIME DILATION DUE TO MOTION OF LOCAL OBSERVER : %.16Lf\n", Dr);
+
+	printf_s("\nTOTAL TIME DILATION OF LOCAL OBSERVER : %.16Lf\n", D);
 
 	if (conBod != -1)
 	{
 		UDIV = CON_U;
 		UDIV *= 2.0;
+		if (bodies[conBod].U_EXPONENT > 12) { UDIV *= powl(10, 12L); }
+		else { UDIV *= powl(10, bodies[conBod].U_EXPONENT); }
 		UDIV /= C_SQ;
+		if (bodies[conBod].U_EXPONENT > 12) { UDIV *= powl(10, bodies[conBod].U_EXPONENT - 12L); }
 
-		VDIV = (long double)powl(bodies[conBod].SPEED, 2);
+		VDIV = powl(bodies[conBod].SPEED, 2);
 		VDIV /= C_SQ;
 	}
 	else
@@ -303,25 +168,23 @@ int main()
 		VDIV = 0.0;
 	}
 
-	RDIV = (double)powl(conSp, 2);
+	RDIV = powl(conSp, 2);
 	RDIV /= C_SQ;
 	RDIV *= 1.0 / (1.0 - UDIV);
 
 	Dg = 1.0 / sqrt(1.0 - UDIV);
 	Dv = 1.0 / sqrt(1.0 - VDIV);
 	Dr = 1.0 / sqrt(1.0 - RDIV);
-
-	cout << "\nTIME DILATION DUE TO GRAVITY AT LOCATION OF CONTROL OBSERVER : " << DToA(Dg) << endl;
-	cout << "TIME DILATION DUE TO MOTION OF CONTROL BODY : " << DToA(Dv) << endl;
-	cout << "TIME DILATION DUE TO MOTION OF CONTROL OBSERVER : " << DToA(Dr) << endl;
-
 	DCon = 1.0 / sqrt(fabs(1.0 - UDIV - VDIV - RDIV));
 
-	cout << "\nTOTAL TIME DILATION OF CONTROL OBSERVER : " << DToA(DCon) << endl;
+	printf_s("\nTIME DILATION DUE TO GRAVITY AT LOCATION OF CONTROL OBSERVER : %.16Lf\n", Dg);
+	printf_s("TIME DILATION DUE TO MOTION OF CONTROL BODY : %.16Lf\n", Dv);
+	printf_s("TIME DILATION DUE TO MOTION OF CONTROL OBSERVER : %.16Lf\n", Dr);
 
-	cout << "\nTOTAL DIFFERENCE BETWEEN OBSERVERS : " << DToA(fabs(DCon - D)) << endl;
+	printf_s("\nTOTAL TIME DILATION OF CONTROL OBSERVER : %.16Lf\n", DCon);
+	printf_s("\nTOTAL DIFFERENCE BETWEEN OBSERVERS : %.16Lf\n", fabs(DCon - D));
 
-	getline(cin, sIn);
+	cin.get();
 
 	return 0;
 }
